3071808f9b65da0ac4c25c20661bbbf731275e6e
Irfan Ashraf <realirfanashraf@gmail.com>
2024-02-08 15:55:06 +0530

    db connect

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import morgan from "morgan";
 import nocache from "nocache";
+import {connectDB} from "./server/connection/database.js"
+
+
 
 
 dotenv.config()
+connectDB()
 const app = express();
 
 app.get('/', (req, res) => {
---
+++ backend/server/connection/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+const mongo_uri = process.env.MONGO_URI
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(mongo_uri);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+    }
+};
