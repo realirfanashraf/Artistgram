0d62bb633d08599a3487e13d46b9dd5d704c1ceb
Irfan Ashraf <realirfanashraf@gmail.com>
2024-03-19 16:57:58 +0530

    user block middleware

--- backend/server/router/userRoutes/uploadRoutes.js
+++ backend/server/router/userRoutes/uploadRoutes.js
@@ -3,12 +3,13 @@ const route = express.Router()
 import { changeProfilePicture, editProfile, newPost, posts,followUser } from '../../controller/userControllers/uploadController.js'
 import { authenticateUser } from '../../middleware/authMiddleware.js'
 import authorize from '../../middleware/authorize.js'
+import isBlocked from '../../middleware/isBlocked.js'
 
 
-route.post('/changeProfilePicture', authenticateUser,authorize('user'), changeProfilePicture)
-route.post('/editProfile', authenticateUser,authorize('user'), editProfile)
-route.post('/newPost', authenticateUser, authorize('user'),newPost)
-route.get('/posts/:id', authenticateUser, authorize('user'),posts)
-route.post('/followUser',authenticateUser,authorize('user'),followUser)
+route.post('/changeProfilePicture', authenticateUser,authorize('user'),isBlocked,changeProfilePicture)
+route.post('/editProfile', authenticateUser,authorize('user'),isBlocked,editProfile)
+route.post('/newPost', authenticateUser, authorize('user'),isBlocked,newPost)
+route.get('/posts/:id', authenticateUser, authorize('user'),isBlocked,posts)
+route.post('/followUser',authenticateUser,authorize('user'),isBlocked,followUser)
 
 export default route
