1f7929529581db052a14e583c2228c112607e8f2
Irfan Ashraf <realirfanashraf@gmail.com>
2024-04-01 15:33:16 +0530

    adminauth middleware and seperate socket code from server

--- backend/server/router/adminRoutes/actionRoute.js
+++ backend/server/router/adminRoutes/actionRoute.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { handleBlockUser , blockPost,addEvent,deleteEvent } from '../../controller/adminControllers/actionController.js'
 import authorize from '../../middleware/authorize.js'
+import {authenticateAdmin} from '../../middleware/authMiddleware.js'
 const route = express.Router()
 
 
-route.post('/handleBlockUser/:userId',authorize('admin'), handleBlockUser)
-route.post('/blockPost/:postId', authorize('admin'), blockPost)
-route.post('/addEvent',authorize('admin'),addEvent)
-route.post('/deleteEvent/:eventId',authorize('admin'),deleteEvent)
+route.post('/handleBlockUser/:userId',authorize('admin'),authenticateAdmin, handleBlockUser)
+route.post('/blockPost/:postId', authorize('admin'),authenticateAdmin, blockPost)
+route.post('/addEvent',authorize('admin'),authenticateAdmin,addEvent)
+route.post('/deleteEvent/:eventId',authorize('admin'),authenticateAdmin,deleteEvent)
 
 export default route
