717e62ca7708a601e923e88e409fb4b0049d0c8f
Irfan Ashraf <realirfanashraf@gmail.com>
2024-04-08 11:38:51 +0530

    config file with paths

---
+++ frontend/src/config.js
@@ -0,0 +1,21 @@
+export const routePaths = {
+    intro: '/',
+    signIn: '/signin',
+    signUp: '/signup',
+    forgotPassword: '/forgotPassword',
+    home: '/home',
+    profile: '/profile',
+    inbox: '/inbox',
+    event: '/event',
+    remoteUserProfile: '/remoteUserProfile/:userId',
+}; 
+
+export const adminRoutePaths = {
+    adminLogin: '/admin',
+    adminDashboard: '/admin/dashboard',
+    userManagement: '/admin/userManagement',
+    postManagement: '/admin/postManagement',
+    eventManagement: '/admin/eventManagement',
+    adminProfile: '/admin/adminProfile'
+  };
+  
