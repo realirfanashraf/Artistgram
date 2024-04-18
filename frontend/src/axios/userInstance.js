6a5b32665e6db1e6f6e9dabdc51486410e873d7d
Irfan Ashraf <realirfanashraf@gmail.com>
2024-04-18 10:45:27 +0530

    network

--- frontend/src/axios/userInstance.js
+++ frontend/src/axios/userInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../redux/store.js';
 
 const Axios = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: import.meta.env.VITE_SERVER_URL || 'https://artistgram.online',
   withCredentials: true
 });
 
